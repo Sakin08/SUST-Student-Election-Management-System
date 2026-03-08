76d92d870330890391c9fcc35d3e032177319eba
sakin08 <2021331008@student.sust.edu>
2026-03-09 00:22:02 +0600

    updated

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL?.trim(),
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
