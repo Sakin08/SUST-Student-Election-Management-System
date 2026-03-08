f1015029de93a0514f65085ad8630b5cd83ad6c1
sakin08 <2021331008@student.sust.edu>
2026-03-09 01:00:21 +0600

    updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001";
+const API = process.env.REACT_APP_API_URL || "http://localhost:5001";
+export const API_URL = API;
+export default API;
